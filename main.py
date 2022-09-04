3167746ebd884833465cd1f0fad196f526863ba2
Maxim <flipsidebeat@gmail.com>
2022-09-04 13:21:54 +0300

    0.1.0

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import time, random
+
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-urls = ['https://kirov.hh.ru/search/vacancy?text=django', 'https://kirov.hh.ru/search/vacancy?text=python']  # Какие url ищутся
+
 driver = webdriver.Firefox()
+
+name_list = []
+url_list = []
+dictionary = {}
 
 def open_hh():
     """Ищет названия вакансий"""
+    search_name = ['django', 'python', 'selenium', 'sql', 'backend']  # по каким тегам ищутся вакансии
 
-    for url in urls:
-        print(url)
-        driver.get(url=url)
+    for url in search_name:
+        driver.get(f'https://kirov.hh.ru/search/vacancy?text={url}')
         time.sleep(random.randrange(5, 10))
 
         driver.find_elements(By.CLASS_NAME, 'bloko-link')
         elements = driver.find_elements(By.TAG_NAME, 'a')
         for element in elements:
-            if ('Python' in element.text) or ('Django' in element.text):
-                print(element.text)
 
+            if ('Python' in element.text) or ('Django' in element.text):
+                name = element.text  # выводит текст названия вакансии
+                url = element.get_attribute('href')  # находит ссылки на вакансии
+                dictionary[name] = url
+
+    print(dictionary)
     driver.close()
+
 
 open_hh()
