3f79de52177c9373ce707c7798480e4adb46d010
Maxim <flipsidebeat@gmail.com>
2022-09-10 17:00:01 +0300

    0.1.3.2

--- main.py
+++ main.py
@@ -7,25 +7,27 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 import psycopg2
 from config import host, user, password, db_name
 
 
 driver = webdriver.Firefox()
 
-class Search_vacancies:
+
+class SearchVacancies:
 
     def __init__(self):
         self.pages = None
         self.counter = 1  # счетчик, нужен для вписывания элемента в определенную строку таблицы
-        self.start_bd()
 
     def parsing_names(self):
         """Ищет названия вакансий и ссылки на них
-        После этого сохраняет в таблицу Excel"""
+        После этого сохраняет в базу данных PostgreSQL"""
 
+        # db.delete_table()  # deleting a table
+        # db.create_table()  # creating a table
         search_name = ['django']  # по каким тегам ищутся вакансии
+        db.start_db()
 
         for url in search_name:
             driver.get(f'https://kirov.hh.ru/search/vacancy?text={url}')  # вписывает в поле слово вакансии
@@ -45,19 +47,16 @@ class Search_vacancies:
                         self.counter += 1  # счетчик +1 каждый раз
                         name = element.text  # выводит текст названия вакансии
                         url = element.get_attribute('href')  # находит ссылки на вакансии
-                        with self.connection.cursor() as cursor:
-                            cursor.execute(
-                                f"""INSERT INTO vacancies (vacancy_name, link_to_vacancy) VALUES
-                                ('{name}', '{url}');"""
-                            )
+                        db.insert_date(name, url)  # записывает
 
-                            print('[INFO] Data was successfully inserted')
-
-        driver.close()  # закрывает браузер
+        driver.close()  # closing the browser
+        db.close_db()  # close connection with PostgreSQL
 
     def amount_pages(self):
         """Ищет количество страниц на сайте"""
+
         driver.find_element(By.CLASS_NAME, 'pager')  # ищет элемент класса (чтобы посчитать количество страниц)
+
         # записывает в переменную количество страниц на сайте
         pages = driver.find_element(By.CSS_SELECTOR,
                                     '#HH-React-Root > div > div.HH-MainContent.HH-Supernova-MainContent > div.main-content > div > div:nth-child(3) > div.sticky-sidebar-and-content--NmOyAQ7IxIOkgRiBRSEg > div.bloko-column.bloko-column_xs-4.bloko-column_s-8.bloko-column_m-9.bloko-column_l-13 > div > div.bloko-gap.bloko-gap_top > div > span:nth-child(6) > span.pager-item-not-in-short-range > a > span')
@@ -66,42 +65,85 @@ class Search_vacancies:
 
     def transition_to_page(self):
         """Переходит на каждую страницу на сайте"""
+
         for i in range(self.pages):  # переходит с одной ссылки (страницы) на другую
             driver.get(f'https://kirov.hh.ru/search/vacancy?text=django&from=suggest_post&salary=&clusters=true&ored_clusters=true&enable_snippets=true&page={i}&hhtmFrom=vacancy_search_list')
 
-    def start_bd(self):
+    def transition_to_links(self):
+        pass
+
+
+class DataBase:
+
+    def __init__(self):
+        self.connection = None
+        self.start_db()
+
+    def start_db(self):
+        """Connecting to PostgreSQL"""
 
         try:
-            #
+            # connecting to PostgreSQL
             self.connection = psycopg2.connect(
                 host=host,
                 user=user,
                 password=password,
                 database=db_name
             )
-            self.connection.autocommit = True
+            self.connection.autocommit = True  # enable autosave data
 
+            # writing a version PostgreSQL
             with self.connection.cursor() as cursor:
                 cursor.execute('SELECT version();')
                 print(f'Server version: {cursor.fetchone()}')
 
         except Exception as _ex:
             print('[INFO] Error while working with PostgreSQL', _ex)
-            self.connection.close()
+            self.connection.close()  # closing the PostgreSQL
+
+    def create_table(self):
+        """Creating a new table"""
+
+        # create a new table
+        with self.connection.cursor() as cursor:
+            cursor.execute(
+                """CREATE TABLE vacancies(
+                    id serial PRIMARY KEY,
+                    vacancy_name varchar(100) NOT NULL,
+                    link_to_vacancy text NOT NULL);"""
+            )
+            print('[INFO] Table created successfully')
+        
+    def insert_date(self, name, url):
+        """Insert data into table"""
+
+        with self.connection.cursor() as cursor:
+            cursor.execute(
+                f"""INSERT INTO vacancies (vacancy_name, link_to_vacancy) VALUES
+                ('{name}', '{url}');"""
+            )
+            print('[INFO] Data was successfully inserted')
+
+    def delete_table(self):
+        """Deleting a table"""
+
+        with self.connection.cursor() as cursor:
+            cursor.execute(f"""DROP TABLE vacancies;""")
+            print('[INFO] Table was deleted')
+
+    def close_db(self):
+        """Close connection with PostgreSQL"""
+
+        self.connection.close()
+        print('[INFO] PostgreSQL connection closed')
 
 
-search = Search_vacancies()
+
+
+search = SearchVacancies()
+db = DataBase()
 
 if __name__ == '__main__':
     search.parsing_names()
 else:
     print('ERROR')
-
-
-# except Exception as _ex:
-#     print('[INFO] Error while working with PostgreSQL', _ex)
-#
-# finally:
-#     if connection:
-#         connection.close()
-#         print('[INFO] PostgreSQL connection closed')
