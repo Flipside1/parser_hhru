1c1898d2c6060980fe94fce5e3cd3b5c0ee86fc9
Maxim <flipsidebeat@gmail.com>
2022-09-10 22:43:11 +0300

    0.1.3.4.0

--- main.py
+++ main.py
@@ -11,17 +11,13 @@ import psycopg2
 from config import host, user, password, db_name
 
 
-driver = webdriver.Firefox()
-
 string = None
-link = None
-
 
 class SearchVacancies:
 
     def __init__(self):
+        self.driver = webdriver.Firefox()
         self.pages = None
-        self.counter = 1  # счетчик, нужен для вписывания элемента в определенную строку таблицы
 
     def parsing_names(self):
         """Ищет названия вакансий и ссылки на них
@@ -35,40 +31,39 @@ class SearchVacancies:
             self.transition_to_links()
 
             for url in search_name:
-                driver.get(f'https://kirov.hh.ru/search/vacancy?text={url}')  # вписывает в поле слово вакансии
+                self.driver.get(f'https://kirov.hh.ru/search/vacancy?text={url}')  # вписывает в поле слово вакансии
                 time.sleep(random.randrange(5, 10))
 
                 self.amount_pages()  # считает количество страниц
 
                 for page in range(self.pages):  # переходит с одной страницы на другую
-                    driver.get(
+                    self.driver.get(
                         f'https://kirov.hh.ru/search/vacancy?text=django&from=suggest_post&salary=&clusters=true&ored_clusters=true&enable_snippets=true&page={page}&hhtmFrom=vacancy_search_list')
 
-                    driver.find_elements(By.CLASS_NAME, 'bloko-link')  # ищет класс, в котором содержатся названия вакансий
-                    elements = driver.find_elements(By.TAG_NAME, 'a')  # ищет элементы с тегом "а"
+                    self.driver.find_elements(By.CLASS_NAME, 'bloko-link')  # ищет класс, в котором содержатся названия вакансий
+                    elements = self.driver.find_elements(By.TAG_NAME, 'a')  # ищет элементы с тегом "а"
 
                     for element in elements:  # перебирает все элементы, которые спарсились
                         if ('Python' in element.text) or ('Django' in element.text):  # если в названии вакансии есть определенные слова
-                            self.counter += 1  # счетчик +1 каждый раз
                             name = element.text  # выводит текст названия вакансии
                             url = element.get_attribute('href')  # находит ссылки на вакансии
                             db.insert_date(name, url)  # записывает
 
-            driver.close()  # closing the browser
+            self.driver.close()  # closing the browser
             db.close_db()  # close connection with PostgreSQL
 
         except Exception as _ex:
-            driver.close()
+            self.driver.close()
             db.close_db()
             print(f"[ERROR] {_ex}")
 
     def amount_pages(self):
         """Ищет количество страниц на сайте"""
 
-        driver.find_element(By.CLASS_NAME, 'pager')  # ищет элемент класса (чтобы посчитать количество страниц)
+        self.driver.find_element(By.CLASS_NAME, 'pager')  # ищет элемент класса (чтобы посчитать количество страниц)
 
         # записывает в переменную количество страниц на сайте
-        pages = driver.find_element(By.CSS_SELECTOR,
+        pages = self.driver.find_element(By.CSS_SELECTOR,
                                     '#HH-React-Root > div > div.HH-MainContent.HH-Supernova-MainContent > div.main-content > div > div:nth-child(3) > div.sticky-sidebar-and-content--NmOyAQ7IxIOkgRiBRSEg > div.bloko-column.bloko-column_xs-4.bloko-column_s-8.bloko-column_m-9.bloko-column_l-13 > div > div.bloko-gap.bloko-gap_top > div > span:nth-child(6) > span.pager-item-not-in-short-range > a > span')
         self.pages = int(pages.text)  # преобразовывает текст в числовой формат
         print(self.pages)
@@ -77,18 +72,37 @@ class SearchVacancies:
         """Переходит на каждую страницу на сайте"""
 
         for i in range(self.pages):  # переходит с одной ссылки (страницы) на другую
-            driver.get(f'https://kirov.hh.ru/search/vacancy?text=django&from=suggest_post&salary=&clusters=true&ored_clusters=true&enable_snippets=true&page={i}&hhtmFrom=vacancy_search_list')
+            self.driver.get(f'https://kirov.hh.ru/search/vacancy?text=django&from=suggest_post&salary=&clusters=true&ored_clusters=true&enable_snippets=true&page={i}&hhtmFrom=vacancy_search_list')
 
     def transition_to_links(self):
         """Goes to each vacancy by links from the database
         and parses data from it"""
-        db.count_strings()
 
-        for num in range(1, string+1):
-            db.following_a_link(num)
-            print('1')
-            driver.get(link)
-            print('2')
+        # iterates over all links in a table
+        for num in range(1, 11):
+            print(num)
+            self.driver.get(db.following_a_link(num))  # clicks to links for each vacancy in a table
+            self.driver.find_element(By.CLASS_NAME, 'g-user-content')
+            elements = self.driver.find_elements(By.CSS_SELECTOR, '#HH-React-Root > div > div.HH-MainContent.HH-Supernova-MainContent > div.main-content > div > div > div > div > div.bloko-column.bloko-column_container.bloko-column_xs-4.bloko-column_s-8.bloko-column_m-12.bloko-column_l-10 > div:nth-child(3) > div > div > div:nth-child(1) > div')
+
+            for element in elements:
+                element = element.text.lower()
+                name = db.vacancy_name(num).lower()
+
+                print(name)
+
+                if ('стажер' in [name or element]) or ('trainee' in [name or element]):
+                    print('trainee')
+
+                if ('junior' in (name or element)) or ('джуниор' in (name, element)) or ('джун' in (name, element)):
+                    print('junior')
+
+                if ('middle' in (name or element)) or ('mid' in (name, element)) or ('мидл' in (name, element)) or ('миддл' in (name, element)):
+                    print('middle')
+
+                if ('senior' in [name or element]) or ('сеньор' in [name or element]) or ('синьор' in [name or element]):
+                    print('senior')
+
             time.sleep(random.randrange(3, 7))
 
 
@@ -133,19 +147,24 @@ class DataBase:
     def count_strings(self):
         """Counts the number of rows in a table"""
 
-        global string
         self.cursor.execute("SELECT count(*) FROM vacancies;")
         strings = self.cursor.fetchone()
-        string = int(strings[0])  # number of rows in a table
+        return int(strings[0])  # number of rows in a table
 
     def following_a_link(self, num):
 
-        global link
-
         self.cursor.execute(f"SELECT link_to_vacancy FROM vacancies WHERE id = '{num}';")
         links = self.cursor.fetchone()
-        link = links[0]
-        print(link)
+        return links[0]
+
+
+
+    def vacancy_name(self, num):
+        self.cursor.execute(f"SELECT vacancy_name FROM vacancies WHERE id = '{num}';")
+        name = self.cursor.fetchone()
+        return name[0]
+
+
 
     def create_table(self):
         """Creating a new table"""
@@ -155,7 +174,11 @@ class DataBase:
             """CREATE TABLE vacancies(
                 id serial PRIMARY KEY,
                 vacancy_name varchar(100) NOT NULL,
-                link_to_vacancy text NOT NULL);"""
+                link_to_vacancy text NOT NULL,
+                trainee text,
+                junior text,
+                middle text,
+                senior text);"""
         )
         print('[INFO] Table created successfully')
 
